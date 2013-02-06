31c0c94559ac8f22f9effc3ca40559c64d8e7447
John Bender <john+gittest@generalui.com>
2013-02-05 16:32:44 -0800

    second version

---
+++ bestapp2/bestapp2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  bestapp2
+//
+//  Created by Testy McTesterson on 2/5/13.
+//  Copyright (c) 2013 Testy McTesterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
