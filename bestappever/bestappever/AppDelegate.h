d3c6ea8fcc264931b116950974bddca6fb216479
John Bender <john+gittest@generalui.com>
2013-02-05 16:29:53 -0800

    my first commit

---
+++ bestappever/bestappever/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bestappever
+//
+//  Created by Testy McTesterson on 2/5/13.
+//  Copyright (c) 2013 Testy McTesterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ bestappever/bestappever/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  bestappever
+//
+//  Created by Testy McTesterson on 2/5/13.
+//  Copyright (c) 2013 Testy McTesterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
